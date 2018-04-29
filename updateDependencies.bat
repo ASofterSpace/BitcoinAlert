a0fe713edf8456266f342f660539c4dd02e2e408
Moya <moyaccercchi@hotmail.de>
2018-04-29 16:20:57 +0200

    Improve dependency updating script

--- updateDependencies.bat
+++ updateDependencies.bat
@@ -3,9 +3,8 @@ cd app\src\main\java\com\asofterspace
 rd /s /q toolbox
 
 md toolbox
-cd toolbox
 
-copy ..\..\..\..\..\..\..\..\Toolbox-Java\src\com\asofterspace\toolbox\Utils.java Utils.java
-echo d | xcopy /s ..\..\..\..\..\..\..\..\Toolbox-Java\src\com\asofterspace\toolbox\web web
+copy "..\..\..\..\..\..\..\Toolbox-Java\src\com\asofterspace\toolbox\*.java" "toolbox"
+copy "..\..\..\..\..\..\..\Toolbox-Java\src\com\asofterspace\toolbox\web\*.*" "toolbox\web"
 
 pause
